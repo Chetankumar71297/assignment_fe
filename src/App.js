41ae7be4576dacddb6c3945d4a4c86e4dc2b78a1
Chetan kumar <chetankumar71297@gmail.com>
2024-05-04 23:53:21 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,33 +4,26 @@ import {
   Route,
   Navigate,
 } from "react-router-dom";
+import PrivateRoute from "./components/PrivateRoute";
 import Home from "./pages/home";
 import Login from "./pages/login";
 import Register from "./pages/register";
-import { useSelector } from "react-redux";
+//import { useSelector } from "react-redux";
 import User from "./pages/user";
 
 function App() {
-  const { user } = useSelector((state) => state.user);
-  const { token } = user;
+  /*const { user } = useSelector((state) => state.user);
+  const { token } = user;*/
   return (
     <div className="dark">
       <Router>
         <Routes>
-<Route path="/user/:userId" element={!token ? <User /> : <Navigate to="/" />} />
-          <Route
-            path="/"
-            element={token ? <Home /> : <Navigate to="/login" />}
-          />
-          <Route
-            path="/login"
-            element={!token ? <Login /> : <Navigate to="/" />}
-          />
-          <Route
-            path="/register"
-            element={!token ? <Register /> : <Navigate to="/" />}
-          />
-          
+          <Route path="/" element={<PrivateRoute />}>
+            <Route path="/" element={<Home />} />
+          </Route>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={ <Register /> } />
+          <Route path="/user/:userId" element={ <User /> } />
         </Routes>
       </Router>
     </div>
